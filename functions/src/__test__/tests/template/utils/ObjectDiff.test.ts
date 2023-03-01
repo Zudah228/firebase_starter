c1241cdf5fc1e28ca4136b3fa3c46763a0c52162
Zudah228 <rezel95.hin@gmail.com>
2023-03-01 15:16:43 +0900

    object diff の追加

---
+++ functions/src/__test__/tests/template/utils/ObjectDiff.test.ts
@@ -0,0 +1,66 @@
+import { objectDiff } from "$src/utils/ObjectDiff";
+
+describe("ObjectDiff の動作テスト", () => {
+  test("プリミティブ型の diff", () => {
+    const objectA = {
+      name: "名前",
+      age: 10,
+    };
+    const objectB = {
+      name: "名前",
+      age: 20,
+    };
+
+    const diff = objectDiff().keys(["age", "name"]).diff(objectA, objectB);
+
+    expect(Object.keys(diff).length).toBe(1);
+    expect(diff).toMatchObject({ age: 20 });
+    expect(diff.name).toBeUndefined();
+  });
+  test("Date型の diff", () => {
+    const now = new Date();
+    const future = new Date(now);
+    future.setDate(future.getDate() + 1);
+
+    const objectA = {
+      name: "名前",
+      date: now,
+    };
+    const objectB = {
+      name: "名前",
+      date: future,
+    };
+
+    const diff = objectDiff<{ name: string; date: Date }>().keys(["date", "name"]).diff(objectA, objectB);
+
+    expect(Object.keys(diff).length).toBe(1);
+    expect(diff).toMatchObject({ date: future });
+    expect(diff.name).toBeUndefined();
+  });
+  test("ネストした Record 型の diff", () => {
+    const now = new Date();
+    const future = new Date(now);
+    future.setDate(future.getDate() + 1);
+
+    const objectA = {
+      name: "名前",
+      record: {
+        age: 10,
+        nickname: "A",
+      },
+    };
+    const objectB = {
+      name: "名前",
+      record: {
+        age: 10,
+        nickname: "B",
+      },
+    };
+
+    const diff = objectDiff().keys(["record", "name"]).diff(objectA, objectB);
+
+    expect(diff).toMatchObject({ record: { nickname: "B" } });
+    expect(diff.record.age).toBeUndefined();
+    expect(diff.name).toBeUndefined();
+  });
+});
---
+++ functions/src/utils/ObjectDiff.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as deepObjectDiff from "deep-object-diff";
+
+/**
+ *
+ * @param originalObj
+ * @param updatedObj
+ * @returns
+ */
+export function objectDiff<T = any>() {
+  return new ObjectDiffBuilder<T>();
+}
+
+type FieldsOf<T> = (keyof Partial<T>)[];
+type ExtractKeys<T, Keys extends FieldsOf<T>> = { [P in Extract<keyof T, Keys[number]>]: T[P] };
+
+/**
+ * 比較する key の割り当て
+ */
+class ObjectDiffBuilder<T = any> {
+  keys = <Keys extends FieldsOf<T>>(keys: Keys): ObjectDiff<T, Keys> => {
+    return new ObjectDiff<T, Keys>();
+  };
+}
+
+/** */
+class ObjectDiff<T, Keys extends FieldsOf<T>> {
+  diff(originalObj: object, updatedObj: object): Partial<ExtractKeys<T, Keys>> {
+    return deepObjectDiff.diff(originalObj, updatedObj) as Partial<ExtractKeys<T, Keys>>;
+  }
+}

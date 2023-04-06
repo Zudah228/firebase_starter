a9d5886f0060449dcbf54b7a8294ab1fd5ed223c
Zudah228 <rezel95.hin@gmail.com>
2023-04-06 18:35:00 +0900

    ファイルの移動

--- functions/src/presentation/interfaces/AuthTrigger.ts
+++
@@ -1,7 +0,0 @@
-import { EventContext } from "firebase-functions/v1";
-import { UserRecord } from "firebase-functions/v1/auth";
-
-/** */
-export abstract class AuthOnCreateTrigger {
-  abstract onCreate: (user: UserRecord, context: EventContext) => PromiseLike<void> | void;
-}
